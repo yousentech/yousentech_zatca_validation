cf935e08b8e0b6a91c67d5e270182a0bcdc46f9e
yousentech <83219678+yousentech@users.noreply.github.com>
2025-11-30 13:06:01 +0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'yousentech_zatca_validation',
-	'version': '13.0.1.0.0',
+	'version': '17.0.1.0.0',
 	'summary': 'yousentech_zatca_validation',
 	'category': 'Tools',
 	'author': 'Developers team',
